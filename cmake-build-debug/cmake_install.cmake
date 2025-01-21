6c337d8e27cb32affb2b0c1b539ad75b3a59330b
Francesco0905 <fraespo295@gmail.com>
2025-01-21 18:03:06 +0100

    boh

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/francesco/Desktop/Uni/Primo anno/Programmazione/Progetto Snake/SnakeGame
+# Install script for directory: /Users/francesco/Desktop/Snake
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "/Users/francesco/Desktop/Uni/Primo anno/Programmazione/Progetto Snake/SnakeGame/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/francesco/Desktop/Snake/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
